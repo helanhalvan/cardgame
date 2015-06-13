e50dfa150b5eda4611ed8163bf3b4d3eff6a6a5d
sten <daesch@kth.se>
2015-06-13 12:10:39 +0200

    asd

--- erlangModules/game.erl
+++ erlangModules/game.erl
@@ -7,10 +7,16 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([]).
+-export([start/1]).
 
-
-
+start(Options)->
+	case opts(Options) of
+		fail -> error;
+		{BoardOpts,PlayerOpts}->
+			Sup=self(),
+			Board={board, fun()->board:start(BoardOpts) end,temporary,brutal_kill,supervisor,dynamic},
+			supervisor:start_child(Sup,Board)
+	end.
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
